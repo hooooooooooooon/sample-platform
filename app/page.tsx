07a90064bfd0afadaab886e92e692cbda407dbf6
hooooooooooooon <douglaskim91@gmail.com>
2024-03-27 02:49:41 +0900

    TailwindCSS Initialized

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <div>
+        <div>
+          <div>
+            <span>In trai</span>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
