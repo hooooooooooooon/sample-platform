0bef875965d6a6e10137dbf6031b9c13c2a1c8b7
hooooooooooooon <douglaskim91@gmail.com>
2024-03-27 03:17:19 +0900

    Home Screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main>
-      <div>
-        <div>
-          <div>
-            <span>In trai</span>
-          </div>
+    <div className="flex flex-col items-center justify-between min-h-screen p-6">
+      <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
+        <span className="text-9xl">🏢</span>
+        <h1 className="text-4xl">건축</h1>
+        <h2>건축 플랫폼에 오신걸 환영합니다.</h2>
+      </div>
+      <div className="flex flex-col items-center gap-3 w-full">
+        <Link
+          href="/create-account"
+          className="w-full bg-lime-500 text-neutral-900 text-lg font-medium py-2.5 rounded-md text-center hover:bg-lime-400 transition-colors"
+        >
+          시작하기
+        </Link>
+        <div className="flex gap-2">
+          <span>이미 계정이 있나요?</span>
+          <Link href="/login" className="hover:underline">로그인</Link>
         </div>
       </div>
-    </main>
+    </div>
   );
 }
