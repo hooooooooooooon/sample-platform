c7a1ed33af93eaf42879bf129fc0dcbcde2ed028
hooooooooooooon <douglaskim91@gmail.com>
2024-03-28 01:57:54 +0900

    Middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,7 @@
 import { NextRequest } from "next/server";
-import getSession from "./lib/session";
 
-export async function middleware(request: NextRequest) {
-  const session = await getSession();
-  if (request.nextUrl.pathname === "/profile") {
-    return Response.redirect(new URL("/", request.url));
-  }
-}
+export async function middleware(request: NextRequest) {}
+
+export const config = {
+  matcher: ["/((?!api_next/static_next/image\favicon.ico).*)"],
+};
