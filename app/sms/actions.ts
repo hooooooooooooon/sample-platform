fcb5982c4d42f0e825c10233769f768d82ef15d8
hooooooooooooon <douglaskim91@gmail.com>
2024-03-27 15:28:03 +0900

    SMS Validation

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -2,10 +2,36 @@
 
 import z from "zod";
 import validator from "validator";
+import { redirect } from "next/navigation";
 
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine(
+    (phone) => validator.isMobilePhone(phone, "ko-KR"),
+    "잘못된 형식의 휴대폰 번호를 입력하셨습니다."
+  );
 const tokenSchema = z.coerce.number().min(100000).max(999999);
 
+interface ActionState {
+  token: boolean;
+}
 export async function smsLogIn(prevState: any, formData: FormData) {
-  formData.get("token");
+  const phone = formData.get("phone");
+  const token = formData.get("token");
+  if (!prevState.token) {
+    const result = phoneSchema.safeParse(phone);
+    if (!result.success) {
+      return { token: false, error: result.error.flatten() };
+    } else {
+      return { token: true };
+    }
+  } else {
+    const result = tokenSchema.safeParse(token);
+    if (!result.success) {
+      return { token: true };
+    } else {
+      redirect("/");
+    }
+  }
 }
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -5,8 +5,13 @@ import Button from "@/components/button";
 import { useFormState } from "react-dom";
 import { smsLogIn } from "./actions";
 
+const initialState = {
+  token: false,
+  error: undefined,
+};
+
 export default function SMSLogIn() {
-  const [state, dispatch] = useFormState(smsLogIn, null);
+  const [state, dispatch] = useFormState(smsLogIn, initialState);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -14,23 +19,25 @@ export default function SMSLogIn() {
         <h2 className="text-xl">휴대폰 번호로 인증해주세요.</h2>
       </div>
       <form action={dispatch} className="flex flex-col gap-3">
-        <Input
-          name="phone"
-          type="number"
-          placeholder="휴대폰 번호를 입력하세요."
-          required
-          min={100000}
-          max={999999}
-          errors={[]}
-        />
-        <Input
-          name="token"
-          type="number"
-          placeholder="인증번호를 입력하세요."
-          required
-          errors={[]}
-        />
-        <Button text="휴대폰 인증하기" />
+        {state.token ? (
+          <Input
+            name="token"
+            type="number"
+            placeholder="인증번호를 입력하세요."
+            required
+            min={100000}
+            max={999999}
+          />
+        ) : (
+          <Input
+            name="phone"
+            type="text"
+            placeholder="휴대폰 번호를 입력하세요."
+            required
+            errors={state.error?.formErrors}
+          />
+        )}
+        <Button text={state.token ? "휴대폰 인증하기" : "인증번호 전송하기"} />
       </form>
     </div>
   );
