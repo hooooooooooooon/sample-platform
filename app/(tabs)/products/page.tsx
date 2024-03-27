0d688d96d7444effc4987417011bead4aecc5d65
hooooooooooooon <douglaskim91@gmail.com>
2024-03-28 03:30:19 +0900

    Product Skeleton

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,4 +1,9 @@
-export default function Product() {
+async function getProducts() {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+}
+
+export default async function Product() {
+  const products = await getProducts();
   return (
     <div>
       <h1 className="text-neutral-200 text-4xl">Product</h1>
