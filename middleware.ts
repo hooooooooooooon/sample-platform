530f4bcd2759f6417df943cb1c8f8fcf05a9d448
hooooooooooooon <douglaskim91@gmail.com>
2024-03-28 02:13:20 +0900

    Authentication Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,32 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import getSession from "./lib/session";
 
-export async function middleware(request: NextRequest) {}
+interface Routes {
+  [key: string]: boolean;
+}
+
+const publicOnlyUrls: Routes = {
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "/create-account": true,
+};
+
+export async function middleware(request: NextRequest) {
+  const session = await getSession();
+  const exists = publicOnlyUrls[request.nextUrl.pathname];
+  // Authentication
+  if (!session.id) {
+    if (!exists) {
+      return NextResponse.redirect(new URL("/", request.url));
+    } else {
+      // if (exists) {
+      //   return NextResponse.redirect(new URL("/products", request.url));
+      // }
+    }
+  }
+}
 
 export const config = {
-  matcher: ["/((?!api_next/static_next/image\favicon.ico).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
