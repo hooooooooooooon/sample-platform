78d55f6cefe16c54e227262589f7cfde3f36cc2f
hooooooooooooon <douglaskim91@gmail.com>
2024-03-27 15:01:12 +0900

    Coerce

--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -1,12 +1,12 @@
-"use client"
+"use client";
 
 import Input from "@/components/input";
 import Button from "@/components/button";
 import { useFormState } from "react-dom";
-import { smsVerification } from "./actions";
+import { smsLogIn } from "./actions";
 
 export default function SMSLogIn() {
-  const [state, dispatch] = useFormState(smsVerification, null)
+  const [state, dispatch] = useFormState(smsLogIn, null);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -14,8 +14,17 @@ export default function SMSLogIn() {
         <h2 className="text-xl">휴대폰 번호로 인증해주세요.</h2>
       </div>
       <form action={dispatch} className="flex flex-col gap-3">
-        <Input name="phone" type="number" placeholder="휴대폰 번호를 입력하세요." required errors={[]} />
-        <Input name="token"
+        <Input
+          name="phone"
+          type="number"
+          placeholder="휴대폰 번호를 입력하세요."
+          required
+          min={100000}
+          max={999999}
+          errors={[]}
+        />
+        <Input
+          name="token"
           type="number"
           placeholder="인증번호를 입력하세요."
           required
