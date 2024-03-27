cb3693d275432d1f43b912b85616e67c54009bda
hooooooooooooon <douglaskim91@gmail.com>
2024-03-27 11:30:31 +0900

    Server Actions

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,15 +3,25 @@ import FormButton from "@/components/form-btn";
 import SocialLogin from "@/components/social-login";
 
 export default function LogIn() {
+  async function handleForm() {
+    "use server";
+  }
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">이메일을 통해 로그인해주세요.</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput type="email" placeholder="이메일" required errors={[]} />
+      <form action={handleForm} className="flex flex-col gap-3">
         <FormInput
+          name="email"
+          type="email"
+          placeholder="이메일"
+          required
+          errors={[]}
+        />
+        <FormInput
+          name="password"
           type="password"
           placeholder="비밀번호"
           required
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
         <span className="text-9xl">🏢</span>
         <h1 className="text-4xl">Alpha.K.A</h1>
-        <h2>건축 플랫폼 알파카에 오신걸 환영합니다.</h2>
+        <h2>건축 플랫폼 알파카에 오신 것을 환영합니다.</h2>
       </div>
       <div className="flex flex-col items-center gap-3 w-full">
         <Link
