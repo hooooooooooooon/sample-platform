39431ed448a3e9bf1b4b2547ddda7cf141ca5d6d
hooooooooooooon <douglaskim91@gmail.com>
2024-03-28 01:25:27 +0900

    Log Out

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -55,6 +55,7 @@ export async function logIn(prevState: any, formData: FormData) {
     if (ok) {
       const session = await getSession();
       session.id = user!.id;
+      await session.save();
 
       redirect("/profile");
     } else {
