a8a4068eaf4be8d1e983074925d22b1b88731d61
hooooooooooooon <douglaskim91@gmail.com>
2024-03-28 17:52:53 +0900

    Product Upload Form

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,7 +1,8 @@
-import ListProduct from "@/components/list-product";
 import ProductList from "@/components/product-list";
 import db from "@/lib/db";
+import { PlusIcon } from "@heroicons/react/24/outline";
 import { Prisma } from "@prisma/client";
+import Link from "next/link";
 
 async function getInitialProducts() {
   const products = await db.product.findMany({
@@ -28,6 +29,13 @@ export default async function Product() {
   const initialProducts = await getInitialProducts();
   return (
     <div>
+      <Link href="/products/add" className="flex gap-5">
+        <div className="pt-5 px-5 flex flex-col w-full">
+          <div className="border-neutral-600 text-neutral-600 border-2 border-dashed rounded-md flex justify-center items-center w-full hover:border-neutral-500 hover:text-neutral-500">
+            <PlusIcon className="h-28" />
+          </div>
+        </div>
+      </Link>
       <ProductList initialProducts={initialProducts} />
     </div>
   );
