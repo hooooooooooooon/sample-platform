261479352039c70a543ccca4ce4ebde6841ff7f1
hooooooooooooon <douglaskim91@gmail.com>
2024-03-29 01:38:21 +0900

    Upload URLs

--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -5,11 +5,12 @@ import Input from "@/components/input";
 import { PhotoIcon } from "@heroicons/react/24/solid";
 import React, { useState } from "react";
 import { useFormState } from "react-dom";
-import { uploadProduct } from "./actions";
+import { getUploadUrl, uploadProduct } from "./actions";
 
 export default function AddProduct() {
   const [preview, setPreview] = useState("");
-  const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const [uploadUrl, setUploadUrl] = useState("");
+  const onImageChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const {
       target: { files },
     } = event;
@@ -19,6 +20,11 @@ export default function AddProduct() {
     const file = files[0];
     const url = URL.createObjectURL(file);
     setPreview(url);
+    const [success, result] = await getUploadUrl();
+    if (success) {
+      const { id, uploadUrl } = result;
+      setUploadUrl(uploadUrl);
+    }
   };
   const [state, action] = useFormState(uploadProduct, null);
   return (
