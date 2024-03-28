340bbf531acde53b7c2d21fc43b52cda33e48828
hooooooooooooon <douglaskim91@gmail.com>
2024-03-29 03:20:25 +0900

    Intercepting Routes

--- middleware.ts
+++ middleware.ts
@@ -20,9 +20,9 @@ export async function middleware(request: NextRequest) {
     if (!exists) {
       return NextResponse.redirect(new URL("/", request.url));
     } else {
-      // if (exists) {
-      //   return NextResponse.redirect(new URL("/products", request.url));
-      // }
+      if (exists) {
+        return NextResponse.redirect(new URL("/home", request.url));
+      }
     }
   }
 }
