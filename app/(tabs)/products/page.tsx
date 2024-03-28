864294174b8734cce6a3e683d10e3870f65f4e8e
hooooooooooooon <douglaskim91@gmail.com>
2024-03-28 10:31:36 +0900

    Product Component

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,12 +1,24 @@
+import ListProduct from "@/components/list-product";
+import db from "@/lib/db";
+
 async function getProducts() {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const products = await db.product.findMany({
+    select: {
+      title: true,
+      price: true,
+      created_at: true,
+      photo: true,
+      id: true
+    }
+  })
+  return products
 }
 
 export default async function Product() {
   const products = await getProducts();
   return (
-    <div>
-      <h1 className="text-neutral-200 text-4xl">Product</h1>
+    <div className="p-5 flex flex-col gap-5">
+      {products.map(product => <ListProduct key={product.id} {...product} />)}
     </div>
   );
 }
