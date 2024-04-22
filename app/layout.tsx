d7c5e177cb922cf6c9367789454c52b01467daa8
hooooooooooooon <douglaskim91@gmail.com>
2024-04-23 04:16:33 +0900

    Made Link Tag in Main Page for PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const APP_DEFAULT_TITLE = "Alpha-KA App"
+const APP_DEFAULT_TITLE = "Alpha-KA App";
 
 export const metadata: Metadata = {
   applicationName: "Alphaka",
@@ -15,8 +15,8 @@ export const metadata: Metadata = {
   description: "알파카 - 건축을 위한 모든 것",
   appleWebApp: {
     capable: true,
-    statusBarStyle: 'default',
-    title: APP_DEFAULT_TITLE
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
   },
   icons: {
     other: [
@@ -41,6 +41,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <link
+        href="/icons/iphonexsmax_splash.png"
+        media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
+        rel="apple-touch-startup-image"
+      />
       <body className={`${inter.className} bg-white text-neutral-800`}>
         {children}
       </body>
