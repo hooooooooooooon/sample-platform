9cafcfbbc37fbe758fdbb19352ae2961ec2136a1
hooooooooooooon <douglaskim91@gmail.com>
2024-04-23 02:37:04 +0900

    First Page Styling Fix

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
               </div>
               <div className="mb-2">
                 <Link href="/sign-in" className="*:text-neutral-800">
-                  <div className="hover:bg-neutral-200 w-full h-10 border-neutral-400 border-[1px] rounded-full font-semibold items-center justify-center content-center">
+                  <div className="h-10 w-full content-center items-center justify-center rounded-full border-[1px] border-neutral-400 font-semibold hover:bg-neutral-200">
                     <span className="text-base">로그인</span>
                   </div>
                 </Link>
